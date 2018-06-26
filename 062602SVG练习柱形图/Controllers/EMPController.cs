6a14dfd3f8e2e2d2f879d8b3e49957b64d1d8001
BokiDogy <wbocai@gmail.com>
2018-06-26 20:39:09 +0800

    添加项目文件。

---
+++ 062602SVG练习柱形图/Controllers/EMPController.cs
@@ -0,0 +1,32 @@
+﻿using SVGStudy.BLL;
+using SVGStudy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _062602SVG练习柱形图.Controllers
+{
+    public class EMPController : Controller
+    {
+        private EmpManager Em = new EmpManager();
+        // GET: EMP
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetAllEmps()
+        {
+            List<Emps> l = Em.GetAllEmps();
+            JsonResult json = Json(l);
+            json.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return json;
+        }
+        public ActionResult GetAll()
+        {
+            List<Emps> l = Em.GetAllEmps();
+            return View(l);
+        }
+    }
+}
