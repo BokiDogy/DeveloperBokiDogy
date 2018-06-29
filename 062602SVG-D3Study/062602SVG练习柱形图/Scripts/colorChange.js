48a02ce5311a72e79400bd76d280d3d1a70efcc5
BokiDogy <wbocai@gmail.com>
2018-06-29 16:13:46 +0800

    062901school

--- 062602SVG-D3Study/062602SVG练习柱形图/Scripts/colorChange.js
+++ 062602SVG-D3Study/062602SVG练习柱形图/Scripts/colorChange.js
@@ -61,10 +61,37 @@ function colorChange2(c1, c2, c3, count) {
 	}
 	return colors;
 }
-		
+
+//计算对比色
+function getComplycolor(hex) {
+	let hsl = hex2hsl(hex);
+	let result = {
+		h: parseInt((hsl.h -120) % 360),
+//		l: 1 - hsl.l,
+		l:hsl.l>0.5?(( 1 - hsl.l)/2):(1-hsl.l/2),
+//		s: hsl.s
+		s: 1
+	}
+	return hsl2hex(result);
+}
+//计算互补色
+function getHucaise(hex) {
+	let hsl = hex2hsl(hex);
+	let result = {
+		h: parseInt((hsl.h +180) % 360),
+//		l: 1 - hsl.l,
+		l:hsl.l>0.5?(( 1 - hsl.l)/2):(1-hsl.l/2),
+		s: hsl.s
+	}
+	return hsl2hex(result);
+}
+
 //十六进制转rgb
 function hexToRgb(hex) {
 	let rgb = [];
+	if(hex.indexOf("#") >= 0) {
+		hex = hex.substring(hex.indexOf("#") + 1);
+	}
 	for(let i = 0; i < 6; i += 2) {
 		rgb.push(parseInt(hex.substr(i, 2), 16));
 	}
@@ -74,4 +101,106 @@ function hexToRgb(hex) {
 function rgbToHex(r, g, b) {
 	let hex = ((r << 16) | (g << 8) | b).toString(16);
 	return "#" + new Array(Math.abs(hex.length - 7)).join("0") + hex;
+}
+
+function hex2hsl(hex) {
+	let rgbs = hexToRgb(hex);
+	let rgb = {
+		r: rgbs[0],
+		g: rgbs[1],
+		b: rgbs[2]
+	};
+	let hsl = rgbToHsl(rgb.r,rgb.b,rgb.b);
+	return hsl;
+}
+
+function hsl2hex(hsl) {
+	let rgb = hslToRgb(hsl.h, hsl.s, hsl.l);
+	let hex = rgbToHex(rgb.r, rgb.g, rgb.b);
+	return hex;
+}
+
+/**
+ * RGB 颜色值转换为 HSL.
+ * 转换公式参考自 http://en.wikipedia.org/wiki/HSL_color_space.
+ * r, g, 和 b 需要在 [0, 255] 范围内
+ * 返回的 h, s, 和 l 在 [0, 1] 之间
+ *
+ * @param   Number  r       红色色值
+ * @param   Number  g       绿色色值
+ * @param   Number  b       蓝色色值
+ * @return  Array           HSL各值数组
+ */
+function rgbToHsl(r, g, b) {
+	r /= 255, g /= 255, b /= 255;
+	var max = Math.max(r, g, b),
+		min = Math.min(r, g, b);
+	var h, s, l = (max + min) / 2;
+
+	if(max == min) {
+		h = s = 0; // achromatic
+	} else {
+		var d = max - min;
+		s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+		switch(max) {
+			case r:
+				h = (g - b) / d + (g < b ? 6 : 0);
+				break;
+			case g:
+				h = (b - r) / d + 2;
+				break;
+			case b:
+				h = (r - g) / d + 4;
+				break;
+		}
+		h /= 6;
+	}
+	let hsl = {
+		h: h * 360,
+		s: s,
+		l: l
+	}
+	return hsl;
+}
+
+
+/**
+ * HSL颜色值转换为RGB. 
+ * 换算公式改编自 http://en.wikipedia.org/wiki/HSL_color_space.
+ * h, s, 和 l 设定在 [0, 1] 之间
+ * 返回的 r, g, 和 b 在 [0, 255]之间
+ *
+ * @param   Number  h       色相
+ * @param   Number  s       饱和度
+ * @param   Number  l       亮度
+ * @return  Array           RGB色值数值
+ */
+function hslToRgb(h, s, l) {
+	h /= 360;
+	var r, g, b;
+
+	if(s == 0) {
+		r = g = b = l; // achromatic
+	} else {
+		var hue2rgb = function hue2rgb(p, q, t) {
+			if(t < 0) t += 1;
+			if(t > 1) t -= 1;
+			if(t < 1 / 6) return p + (q - p) * 6 * t;
+			if(t < 1 / 2) return q;
+			if(t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+			return p;
+		}
+
+		var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		var p = 2 * l - q;
+		r = hue2rgb(p, q, h + 1 / 3);
+		g = hue2rgb(p, q, h);
+		b = hue2rgb(p, q, h - 1 / 3);
+	}
+	let result = {
+		r: Math.round(r * 255),
+		g: Math.round(g * 255),
+		b: Math.round(b * 255)
+	};
+	return result;
 }
